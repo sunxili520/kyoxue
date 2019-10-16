4af6072580e642121b2abbe1dae1b73058cc1f1c
kyoxue <807071983@qq.com>
2019-10-16 14:18:35 +0800

    qqq

---
+++ demo/src/main/java/com/example/demo/DemoConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.filter.Test1Filter;
+import com.example.demo.filter.Test2Filter;
+
+@Configuration
+public class DemoConfiguration {
+
+	@Bean
+	public FilterRegistrationBean<Test1Filter> RegistTest1(){
+		//通过FilterRegistrationBean实例设置优先级可以生效
+		//通过@WebFilter无效
+		FilterRegistrationBean<Test1Filter> bean = new FilterRegistrationBean<Test1Filter>();
+		bean.setFilter(new Test1Filter());//注册自定义过滤器
+		bean.setName("flilter1");//过滤器名称
+		bean.addUrlPatterns("/*");//过滤所有路径
+		bean.setOrder(1);//优先级，最顶级
+		return bean;
+	}
+	@Bean
+	public FilterRegistrationBean<Test2Filter> RegistTest2(){
+		//通过FilterRegistrationBean实例设置优先级可以生效
+		//通过@WebFilter无效
+		FilterRegistrationBean<Test2Filter> bean = new FilterRegistrationBean<Test2Filter>();
+		bean.setFilter(new Test2Filter());//注册自定义过滤器
+		bean.setName("flilter2");//过滤器名称
+		bean.addUrlPatterns("/test/*");//过滤所有路径
+		bean.setOrder(6);//优先级，越低越优先
+		return bean;
+	}
+}
---
+++ demo/src/main/java/com/example/demo/filter/Test2Filter.java
@@ -0,0 +1,23 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class Test2Filter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest request=(HttpServletRequest)arg0;
+		System.out.println("自定义过滤器filter2触发,拦截url:"+request.getRequestURI());
+		arg2.doFilter(arg0,arg1);
+	}
+
+}
