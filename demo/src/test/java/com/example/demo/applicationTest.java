6977f70f37f9153df384d307d74d73c93f60d854
kyoxue <807071983@qq.com>
2019-10-16 15:30:11 +0800

    aaa

--- demo/src/test/java/com/example/demo/applicationTest.java
+++ demo/src/test/java/com/example/demo/applicationTest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,4 +25,11 @@ public class applicationTest {
 		testService.print();
 	}
 
+	@Value("${demo.title}")
+	private String title;
+	
+	@Test
+	public void testProperties()throws Exception{
+		System.out.println(title);
+	}
 }
